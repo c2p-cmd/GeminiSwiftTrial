3ae073831615744b8af6d8da7c18e66989887872
Sharan Thakur <sharandt19@live.com>
2023-12-15 18:57:57 +0530

    Initial Commit

---
+++ GeminiTrial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeminiTrial
+//
+//  Created by Sharan Thakur on 15/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeminiTrial/GeminiTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiTrialApp.swift
+//  GeminiTrial
+//
+//  Created by Sharan Thakur on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GeminiTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
